a1e66d91f249fcf2a025f2b119b712dcaa404a4d
Joguirod <joguicane@gmail.com>
2023-11-28 10:50:40 -0300

    questao 15

---
+++ terceira_atividade/15questao.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "PilhaC.h"
+#include "FilaEmC.h"
+
+int main(void){
+    char s[256];
+    Fila F = fila(256);
+    Pilha P = pilha(256);
+    printf("\nFrase? ");
+    gets(s);
+    for(int i = 0; s[i]; i++){
+        if (isalpha(s[i])){
+            enfileira(toupper(s[i]), F);
+            empilha(toupper(s[i]), P);
+        }
+    }
+    while(!vaziaf(F) && desenfileira(F) == desempilha(P));
+    if (vaziaf(F)) puts("A frase é palindroma");
+    else puts("A frase não é palindroma");
+    destroif(&F);
+    destroip(&P);
+    return 0;
+}
